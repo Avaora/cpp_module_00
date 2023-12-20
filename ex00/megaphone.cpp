8313a8e81d984bf70a50db88f9f39c5e207b9fdf
Yagiz <kalenderiyagiz@gmail.com>
2023-12-20 15:15:20 +0300

    in ex00 while loops changed to for loops and now the code shorter than before

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,24 +2,14 @@
 
 int	main(int argc, char *argv[])
 {
-	int	x;
-	int	y;
-
-	x = 1;
-	y = 0;
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		while ((x < argc) && (argv[x] != NULL))
+		for (int x = 1; (x < argc) && (argv[x] != NULL); x++)
 		{
-			while (argv[x][y] != 0x0)
-			{
+			for (int y = 0; (argv[x][y] != 0x0); y++)
 				std::cout << (char)std::toupper(argv[x][y]);
-				y++;
-			}
-			x++;
-			y = 0;
 		}
 	}
 	std::cout << std::endl;
